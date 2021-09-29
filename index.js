02e454d1e3e6946b7609bd26e4eeb0d58cb5c766
Ilja leyberman <7ilya@gmx.de>
2021-09-29 15:47:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const core = require('@actions/core');
 try {
   const file = core.getInput('file') || 'package.json';
   console.log(`Using ${file}`);
-  const version = JSON.parse(fs.readFileSync(file)).version;
+  
+  //const version = JSON.parse(fs.readFileSync(file)).version;
+  
+  let version = 'latest';
+
+  bg.split('\n').forEach((r) => {
+    if (r.startsWith('version=') || r.startsWith('version =')) {
+      version = r.replace(/(version|=|"| )/g, '');
+    }
+  });
+  
   console.log(version);
   core.setOutput("version", version);
 } catch (error) {
