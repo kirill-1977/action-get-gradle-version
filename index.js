c8ab7d18d3a0b4b39ffaada977f1e18535ceeb86
Ilja leyberman <7ilya@gmx.de>
2021-09-29 17:29:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ try {
   const file = core.getInput('file') || 'package.json';
   console.log(`Using ${file}`);
   const bg = fs.readFileSync(file) + "";
-  console.log(`content of bg: ${bg}`);
-  //const version = JSON.parse(fs.readFileSync(file)).version;
   
   let version = 'latest';
 
